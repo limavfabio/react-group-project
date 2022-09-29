6aaa6dda70f91629af16858c227948c25c260e5f
Ibnballo1 <belloabdullateef035@gmail.com>
2022-09-29 10:01:35 +0100

    Add reserved to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import rocketsReducer from './rockets/rockets';
 import rocketsReducer, { getRockets } from './rockets/rockets';
-
-// const rootReducer = combineReducers({
-//   rocketsReducer,
-// });
 
 const store = configureStore({
   reducer: {
@@ -13,6 +8,5 @@ const store = configureStore({
 });
 
 store.dispatch(getRockets());
-// console.log(store.dispatch(reserved(1)));
 
 export default store;
--- src/views/RocketsView.jsx
+++ src/views/RocketsView.jsx
@@ -2,7 +2,6 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { reserved } from '../app/rockets/rockets';
-// import getRockets from '../app/rockets/rockets';
 
 const state = {
   id: '',
@@ -15,10 +14,7 @@ function RocketsView() {
   const dispatch = useDispatch();
   const [rocket, setRocket] = useState(state);
   const selectors = useSelector((state) => state.rockets);
-  // selectors.map((selector) => console.log(selector.id));
-  // console.log(selectors);
   const handleClick = (id) => {
-    // console.log('Reserved with id number ', id, ' clicked');
     const newState = selectors.map((rocket) => {
       if (rocket.id !== id) {
         return rocket;
@@ -27,36 +23,33 @@ function RocketsView() {
     });
 
     setRocket(() => ({
-      ...rocket, newState
+      ...rocket,
+      newState,
     }));
-    // dispatch(reserved(newState));
+    dispatch(reserved(newState));
   };
 
   return (
     <section>
       {
-        selectors.map((eachRocket) => {
-          console.log(eachRocket.flickr_images[0]);
-
-          return (
-            <div className="eachRocket-Container" key={eachRocket.id}>
-              <div className="rocket-image">
-                <img src={eachRocket.flickr_images[0]} alt="Rocket" />
-              </div>
-              <div className="name_description">
-                <h1>{eachRocket.rocket_name}</h1>
-                <p>{eachRocket.description}</p>
-                <button
-                  className="btn"
-                  type="button"
-                  onClick={() => dispatch(reserved(handleClick(eachRocket.id)))}
-                >
-                  Reserve Rocket
-                </button>
-              </div>
+        selectors.map((eachRocket) => (
+          <div className="eachRocket-Container" key={eachRocket.id}>
+            <div className="rocket-image">
+              <img src={eachRocket.flickr_images[0]} alt="Rocket" />
             </div>
-          );
-        })
+            <div className="name_description">
+              <h1>{eachRocket.rocket_name}</h1>
+              <p>{eachRocket.description}</p>
+              <button
+                className="btn"
+                type="button"
+                onClick={() => handleClick(eachRocket.id)}
+              >
+                Reserve Rocket
+              </button>
+            </div>
+          </div>
+        ))
       }
     </section>
   );
