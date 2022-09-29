6891b293fc8398e02184113d351a19208739b15b
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-09-29 11:51:35 -0300

    Merge the stores into one

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -3,13 +3,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { createLogger } from 'redux-logger';
 import missionsReducer from './Missions/missionsReducer';
+import rocketsReducer, { getRockets } from './rockets/rockets';
 
 const store = configureStore({
   reducer: {
     missions: missionsReducer,
+    rockets: rocketsReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(createLogger()),
 });
+
+store.dispatch(getRockets());
 
 export default store;
