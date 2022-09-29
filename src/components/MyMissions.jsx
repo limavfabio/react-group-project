a856816f2e6ff77ffc1cb642fc4d3125273a2a08
Fábio Vinícius Lima <limavfabio@gmail.com>
2022-09-29 17:18:32 -0300

    Add missions joined layout

--- src/components/MyMissions.jsx
+++ src/components/MyMissions.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import React from 'react';
 import { useSelector } from 'react-redux';
+import style from '../styles/MyMissions.module.css';
 
 const MyMissions = () => {
   const joinedMissions = useSelector((state) =>
@@ -9,9 +10,11 @@ const MyMissions = () => {
   return (
     <div>
       <h1>My Missions</h1>
-      <div>
+      <div className={style.missionsContainer}>
         {joinedMissions.map((mission) => (
-          <div>{mission.mission_name}</div>
+          <div key={mission.mission_id} className={style.mission}>
+            {mission.mission_name}
+          </div>
         ))}
       </div>
     </div>
